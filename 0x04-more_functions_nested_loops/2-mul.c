22b940d297a620f1d70c6d54c8a2bef1805ec5c8
thenunyaonline <nunya.apetsi@gmail.com>
2023-10-12 09:08:30 -0700

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
