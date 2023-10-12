2fb461942ea0aa3107654fc5e63d4b7c9eb4e147
thenunyaonline <nunya.apetsi@gmail.com>
2023-10-12 12:47:59 -0700

    Diagon Alley

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function prints diagonally with '\'
+ * @n: number of times '\' will be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j <= i ; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
