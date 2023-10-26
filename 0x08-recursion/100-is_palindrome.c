0982dc64edb478927ad38c5383996eaebc5d5757
thenunyaonline <nunya.apetsi@gmail.com>
2023-10-25 21:54:23 -0700

    Palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strlen_recursion - generates string lenght
+ * @s: string
+ * Return: lenght of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * compare_string - compares string characters
+ * @s: string
+ * @left: parameter
+ * @right: parameter
+ * Return: int
+ */
+int compare_string(char *s, int left, int right)
+{
+	if (*(s + left) == *(s + right))
+	{
+		if (left == right || left == right + 1)
+			return (1);
+		return (0 + compare_string(s, left + 1, right - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - determines if string is palindrome
+ * @s: string
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (compare_string(s, 0, _strlen_recursion(s) - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,5 +15,7 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int sqrt_a(int a, int b);
 int prime_a(int a, int b);
+int compare_string(char *s, int left, int right);
+int _strlen_recursion(char *s);
 
 #endif
