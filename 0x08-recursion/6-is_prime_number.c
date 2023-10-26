185d94b4b90382132b0a997d70b0b97e29ba0450
thenunyaonline <nunya.apetsi@gmail.com>
2023-10-25 21:08:55 -0700

    Recursive prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * prime_a - calculates prime number
+ * @a: value
+ * @b: divisor
+ * Return: 0
+ */
+
+int prime_a(int a, int b)
+{
+	if (a <= 1 || (a != b && a % b == 0))
+	{
+		return (0);
+	}
+	else if (a == b)
+	{
+		return (1);
+	}
+	return (prime_a(a, b + 1));
+}
+
+/**
+ * is_prime_number - determines prime number
+ * @n: input value
+ * Return: 0 or 1
+ */
+
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int sqrt_a(int a, int b);
+int prime_a(int a, int b);
 
 #endif
