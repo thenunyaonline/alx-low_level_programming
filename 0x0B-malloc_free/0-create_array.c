a6995affe91daced568e093b82b7ec2b54479242
thenunyaonline <nunya.apetsi@gmail.com>
2023-10-31 21:46:35 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: strings of character
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	size_t i;
+	char *ptr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
