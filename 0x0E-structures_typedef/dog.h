a024ab06caea466e8c4fbbe75594599a3b61732a
thenunyaonline <nunya.apetsi@gmail.com>
2023-11-06 22:40:41 -0800

    Struct for dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - structure of dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
