7ed4afa2b8f0d05c95a5948cdc17edbc96f768d9
thenunyaonline <nunya.apetsi@gmail.com>
2023-11-08 06:27:03 -0800

    Print opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - prints opcodes of main function
+ * @a: address of main function
+ * @n:number of bytes
+ * Return: 0
+ */
+
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - prints opcodes of main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *) &main, n);
+	return (0);
+}
