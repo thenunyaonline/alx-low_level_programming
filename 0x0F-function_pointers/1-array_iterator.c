c8299c6a36209c73ea99a62142608df49d315cd4
thenunyaonline <nunya.apetsi@gmail.com>
2023-11-08 04:08:11 -0800

    Array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array iterator - iterates through an array
+ * @array: the array
+ * @size: array size
+ * @action: function pointer
+ * Return: 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(*array);
+		array++;
+	}
+}
