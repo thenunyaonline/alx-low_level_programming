a0fd10122250a5f38fcb2c3c4402f2e3d73e0f30
thenunyaonline <nunya.apetsi@gmail.com>
2023-11-16 22:59:23 -0800

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list of nodes
+ * @head: pointer to first node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+	free(head->str);
+	free(head);
+}
