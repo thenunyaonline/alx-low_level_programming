824dda534f9e43f43850a4c77edc062b63474d9a
thenunyaonline <nunya.apetsi@gmail.com>
2023-11-21 15:37:04 -0800

    return number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - number of elements in a lists
+ * @h: pointer
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
