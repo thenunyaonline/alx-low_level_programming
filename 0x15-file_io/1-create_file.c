91b73ff6d77b6629169a148dfcbd9a47883eacfd
thenunyaonline <nunya.apetsi@gmail.com>
2023-12-01 07:42:59 -0800

    This function creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: prototype
+ * @text_content : content of file
+ * Return: 1 or 0
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
