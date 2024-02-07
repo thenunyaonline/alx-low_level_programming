53b8eaf8c3fee88a3e284af7488a1a062110627b
thenunyaonline <nunya.apetsi@gmail.com>
2024-02-07 14:26:56 -0800

    Hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - function implementing the djb2 algorithm.
+ * @str: string
+ * Return: The calculated hash
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
